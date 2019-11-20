dda4b61cf6130d95dc11920da312c14db168189e
wrightrocket-la <57968214+wrightrocket-la@users.noreply.github.com>
2019-11-20 13:38:31 -0800

    To be used as the script for the Hands-on Lab: Creating a Cronjob to Run a Script Periodically

---
+++ loadavg.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Extract timestamp and the 1, 5, and 15 minute load averages from the uptime command
+uptime | tr ',' ' ' | tr -s ' ' | cut -d' ' -f2,10,11,12
+# Send output of uptime to tr to translate commas to spaces
+# Send that output to tr to squeeze out duplicate spaces
+# Send that to cut to extract the 2, 10, 11, and 12th fields as delimited by spaces
